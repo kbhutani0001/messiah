b27c982cd17144ff4e6522cea0ad28060cba820d
Kartikay Bhutani <kbhutani0001@gmail.com>
2018-12-15 23:20:40 +0530

    Updated api.py: Added dictionary return for Full history

--- src/backend/api.py
+++ src/backend/api.py
@@ -9,6 +9,7 @@ import json
 import os
 import random
 import datetime
+import pandas as pd
 
 CASUALTY_DATA_FILE = "data/random_facts.json"
 
@@ -59,8 +60,11 @@ def get_full_history():
         val = request.args.get(arg)
         db_handle = DBHandler()
         query = db_handle.query(arg, val)
-
-    return jsonify(query)
+    data=pd.DataFrame(query)
+    data.columns=['Deaths', 'Date', 'City', 'Country', 'Severity', 'Source', 'Disaster']
+    data=data.drop(['City', 'Source'], 1)
+    dict = data.to_dict()
+    return (dict)
 
 
 @app.route('/show_random_facts', methods=['GET'])
